1dc77a821eca582f863fc5e71f494eaa975be2d3
hm5650 <hmmm5651@gmail.com>
2026-01-17 11:30:35 +0500

    Create SA2Test_FindTool.lua

---
+++ SA2Test_FindTool.lua
@@ -0,0 +1,5 @@
+local FindTool = {
+    CTween = loadstring(game:HttpGet("https://raw.githubusercontent.com/hm5650/HBSS/refs/heads/main/SA2_CTween.lua"))(),
+    PathFind = loadstring(game:HttpGet("https://raw.githubusercontent.com/hm5650/HBSS/refs/heads/main/SA2_PathFind.lua"))()
+}
+return FindTool
