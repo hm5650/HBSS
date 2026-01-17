3f55eb588f296c3c517156553c64a313ac9607cf
hm5650 <hmmm5651@gmail.com>
2026-01-17 11:24:38 +0500

    Update SA2_Function.lua

--- SA2_Function.lua
+++ SA2_Function.lua
@@ -8,48 +8,46 @@ local mouse = lplr:GetMouse()
 local UserInputService = game:GetService("UserInputService")
 local GetMouseLocation = UserInputService.GetMouseLocation
 
-local functions = {}
-
-functions.GetScreenPosition = function(Vector)
-    local Vec3, OnScreen = WorldToScreen(Camera, Vector)
-    return Vector2.new(Vec3.X, Vec3.Y), OnScreen
-end
-
-functions.IsTool = function(Tool)
-    return Tool:IsA("Tool")
-end
-
-functions.IsAlive = function(Plr)
-    return Plr.Character and Plr.Character:FindFirstChild("Humanoid") and Plr.Character.Humanoid.Health > 0
-end
-
-functions.TeamCheck = function(Plr)
-    return Plr.Team ~= lplr.Team
-end
-
-functions.GetMousePosition = function()
-    return GetMouseLocation(UserInputService)
-end
-
-functions.GetGun = function(Plr)
-    local Character = lplr.Character
-    if not Character then return end
-    for _,v in ipairs(Character:GetChildren()) do
-        if functions.IsTool(v) then
-            return v
+-- Replace the loaded functions with a version that doesn't use WorldToScreen
+local func = {
+    GetScreenPosition = function(Vector)
+        -- Return nil for screen position since we're not using it
+        return nil, false
+    end,
+    
+    IsTool = function(Tool)
+        return Tool:IsA("Tool")
+    end,
+    
+    IsAlive = function(Plr)
+        return Plr.Character and Plr.Character:FindFirstChild("Humanoid") and Plr.Character.Humanoid.Health > 0
+    end,
+    
+    TeamCheck = function(Plr)
+        return Plr.Team ~= lplr.Team
+    end,
+    
+    GetMousePosition = function()
+        return GetMouseLocation(UserInputService)
+    end,
+    
+    GetGun = function(Plr)
+        local Character = lplr.Character
+        if not Character then return end
+        for _,v in ipairs(Character:GetChildren()) do
+            if functions.IsTool(v) then
+                return v
+            end
         end
+    end,
+    
+    HitChance = function(Percentage)
+        Percentage = math.floor(Percentage)
+        local chance = math.floor(Random.new().NextNumber(Random.new(),0,1) * 100) / 100
+        return chance <= Percentage / 100
+    end,
+    
+    Direction = function(Origin, Pos)
+        return (Pos - Origin).Unit * 1000
     end
-end
-
-functions.HitChance = function(Percentage)
-    Percentage = math.floor(Percentage)
-    local chance = math.floor(Random.new().NextNumber(Random.new(),0,1) * 100) / 100
-
-    return chance <= Percentage / 100
-end
-
-functions.Direction = function(Origin, Pos)
-    return (Pos - Origin).Unit * 1000
-end
-
-return functions
+}
