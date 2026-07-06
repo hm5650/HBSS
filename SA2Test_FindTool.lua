003267f9ba18d15c8eda471b9d11fa54cbf64a3b
hm5650 <hmmm5651@gmail.com>
2026-07-06 21:20:42 +0500

    Load CTween and PathFind scripts from GitHub

--- SA2Test_FindTool.lua
+++ SA2Test_FindTool.lua
@@ -1,3 +1,4 @@
+repeat wait() until game:IsLoaded()
 local FindTool = {
     CTween = loadstring(game:HttpGet("https://raw.githubusercontent.com/hm5650/HBSS/refs/heads/main/SA2_CTween.lua"))(),
     PathFind = loadstring(game:HttpGet("https://raw.githubusercontent.com/hm5650/HBSS/refs/heads/main/SA2_PathFind.lua"))()
