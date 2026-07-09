91cf1019bd8d4a1d27096577d542aca31be0c69e
hm5650 <hmmm5651@gmail.com>
2026-07-09 14:36:57 +0500

    fix dat hbss

--- HBSS_New.lua
+++ HBSS_New.lua
@@ -8,7 +8,7 @@
 ⠸⣿⡀⠀⠀⠀⣠⣾⠟⠁⠀⠀⠀⠀⠀⠀
 ⠀⠙⠻⠿⠿⠟⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
            
-           “Im da meme -RNG-est” 
+           “shovel, shovel-est” 
                                            
                                - Gpssickle
 ]]
@@ -45,6 +45,9 @@ local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 loadstring(game:HttpGet("https://raw.githubusercontent.com/hm5650/HBSS/refs/heads/main/HBSS_Loader.lua"))()
 local Alurt = loadstring(game:HttpGet("https://raw.githubusercontent.com/azir-py/project/refs/heads/main/Zwolf/AlurtUI.lua"))()
+local func = loadstring(game:HttpGet("https://raw.githubusercontent.com/hm5650/HBSS/refs/heads/main/SA2_Function.lua"))()
+local FindTool = loadstring(game:HttpGet("https://raw.githubusercontent.com/hm5650/HBSS/refs/heads/main/SA2_FindTool.lua"))()
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
 local function n(opts)
     if typeof(Alurt) == "table" and type(Alurt.CreateNode) == "function" then
@@ -435,8 +438,8 @@ local config = {
         aimbot360LoopTask = nil,
         lastTargetUpdate = 0,
         triggerBotConnection = nil,
-        sa2thing = 0,
-        sa2stuff = 0.03,
+        sa2thing = 0.5,
+        sa2stuff = 0.05,
         spinbotConnection = nil,
         ViewConnection = nil,
         CameraDistance = 8,
@@ -461,13 +464,14 @@ local btntitle = {
     "Gui size decreases",
     "dude",
     "yh",
+    "how graveling of u",
     "rock solid ui",
     "what",
     "version: idk",
     "D:",
     "unclose me NOW!!! D:",
     "just simply cheat through it",
-    "sand.cc",
+    "bowl",
     "gta 6 when?",
     "holy cow",
     "open4robuc",
@@ -777,15 +781,15 @@ local function givename()
         "Gravel.cc",
         "Gravel-est",
         "Gravel-er",
-        "Graaaavel",
-        "Shovel.cc",
+        "Graaaavel.cc",
+        "Gravelly.cc",
         "HBSS.lua",
         "Gravel.com",
-        "Hi! I'm Gravel!",
+        "Hi! I'm Gravel.cc",
         "Gravel enjoyer",
-        "GRAVEL",
-        "g r a v e l",
-        "GravelGravelGravel",
+        "GRAVEL.CC >:D",
+        "holy gravel.cc",
+        "GravelGravelGravel.cc",
         "G.cc",
         "I like gravel",
         "Gravel.cheatcheat",
@@ -2430,8 +2434,6 @@ function respawn(plr)
     end)
 end
 
-local func = loadstring(game:HttpGet("https://raw.githubusercontent.com/hm5650/HBSS/refs/heads/main/SA2_Function.lua"))()
-local FindTool = loadstring(game:HttpGet("https://raw.githubusercontent.com/hm5650/HBSS/refs/heads/main/SA2_FindTool.lua"))()
 local function hasForcefield(character)
     if not character then return false end
     
@@ -7421,7 +7423,6 @@ local function applyClientMaster(state)
 end
 
 -- ui neuron activation starter
-local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 math.randomseed(os.time())
 
 local Window = WindUI:CreateWindow({
@@ -7441,7 +7442,7 @@ local Window = WindUI:CreateWindow({
         ButtonsType = "Default"
     }
 })
-task.wait(0.5)
+task.wait(0.2)
 local rng = function()
     local m = {
         ":0",
@@ -7649,7 +7650,7 @@ local rng2 = function()
 end
 rng2()
 Window:Tag({
-    Title = "YT: @gpssickle\n;3",
+    Title = "wowzerz it's a universal script :o",
     Icon = "github",
     Color = Color3.fromHex("#1c1c1c"),
     Border = true
