2c0cfb97206b2cc703171caef4349830f2a6bde9
Gpssickle! <hmmm5651@gmail.com>
2026-09-12 22:16:03 +0500

    >:3

--- module/SA2/SA2_FindTool.lua
+++ module/SA2/SA2_FindTool.lua
@@ -1,5 +1,5 @@
 local FindTool = {
-    CTween = loadstring(game:HttpGet("https://raw.githubusercontent.com/hm5650/HBSS_C/refs/heads/main/module/SA2/SA2_CTween_C.lua"))(),
-    PathFind = loadstring(game:HttpGet("https://raw.githubusercontent.com/hm5650/HBSS_C/refs/heads/main/module/SA2/SA2_PathFind_C.lua"))()
+    CTween = loadstring(game:HttpGet("https://raw.githubusercontent.com/hm5650/HBSS/refs/heads/main/module/SA2/SA2_CTween.lua"))(),
+    PathFind = loadstring(game:HttpGet("https://raw.githubusercontent.com/hm5650/HBSS/refs/heads/main/module/SA2/SA2_PathFind.lua"))()
 }
 return FindTool
